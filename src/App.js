a1332355f944f223024c173ad312b7118e97b725
Accennovate-office <accennovate.office@gmail.com>
2023-07-31 18:03:44 +0530

    Student ReactJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8080/students`
+        );
+        if (!response.ok) {
+          throw new Error(
+            `This is an HTTP error: The status is ${response.status}`
+          );
+        }
+        let actualData = await response.json();
+        console.log(actualData);
+        setData(actualData);
+        setError(null);
+      } catch(err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }  
+    }
+    getData()
+    // setInterval(() => {
+    //   getData()
+    // }, 1000);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Students List</h1>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+      <ul>
+        {data &&
+          data.map(({ id, name, rollno, gender, dob }) => (
+            <li key={id}>
+              <div style={{display: "flex"}}>
+                <h3>{name}</h3>
+                <h5 className="gender">{gender}</h5>
+              </div>
+              <div style={{display: "flex", justifyContent: "space-between"}}>
+                <h5>Roll No: {rollno}</h5>
+                <p>Date of Birth: {dob}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
